903a4f1931488210905eeca14d756fba3c0fd026
ntltd <nicolas.taltaud@gmail.com>
2023-01-24 17:42:19 +0100

    test: update tests suite

--- src/test/remove-strikethrough-chars.test.js
+++ src/test/remove-strikethrough-chars.test.js
@@ -1,7 +1,6 @@
 import * as utils from '../utils'
 
 test('should return remove strikethrough chars', () => {
-  expect(utils.removeStrikethroughChars('Rhubarb with fresh thyme, basil, green curry, and chocolate plate for only:' +
-    ' 456€ 7̶8̶9̶€̶.'))
+  expect(utils.removeStrikethroughChars('Rhubarb with fresh thyme, basil, green curry, and chocolate plate for only: 456€ 7̶8̶9̶€̶.'))
     .toEqual('Rhubarb with fresh thyme, basil, green curry, and chocolate plate for only: 456€ .')
 })
--- src/test/remove-strikethrough.test.js
+++ src/test/remove-strikethrough.test.js
@@ -1,7 +1,8 @@
 import * as utils from '../utils'
 
 test('should return remove strikethrough', () => {
-  expect(utils.removeStrikethrough('Rhubarb with fresh thyme, basil, green curry, and chocolate plate for only: 456€' +
-    ' (7̶8̶9̶€̶).'))
+  expect(utils.removeStrikethrough('Rhubarb with fresh thyme, basil, green curry, and chocolate plate for only: 456€ (7̶8̶9̶€̶).'))
     .toEqual('Rhubarb with fresh thyme, basil, green curry, and chocolate plate for only: 456€ (789€).')
+  expect(utils.removeStrikethrough('Rhubarb with fresh thyme, basil, green curry, and chocolate plate for only: 456€.'))
+    .toEqual('Rhubarb with fresh thyme, basil, green curry, and chocolate plate for only: 456€.')
 })
